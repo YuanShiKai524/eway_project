6726839ae48ebba5d9a4e069b84f29203aab20ee
yuanshikai524 <gcobc04992@gmail.com>
2023-01-18 14:32:57 +0800

    修改了部分樣式及新增了icons、圖片檔、Header組件(尚未完成)

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className="header-container">
+      <div className="slogan-container">
+        <h2>There is no one</h2>
+        <h2>who loves pain</h2>
+      </div>
+      <div className="whiteWave" />
+    </div>
+  );
+};
+
+export default Header;
