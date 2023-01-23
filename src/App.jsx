5a36e504fd4eb980407c8989fff1953aab80b66c
yuanshikai524 <gcobc04992@gmail.com>
2023-01-23 08:39:36 +0800

    新增並完成燈塔組件

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import Form from './components/Form';
 import Prize from './components/Prize';
+import Lighthouse from './components/Lighthouse';
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Header />
       <Form />
       <Prize />
+      <Lighthouse />
     </div>
   );
 };
---
+++ src/components/Lighthouse.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Lighthouse = () => {
+  return (
+    <div className="lighthouse-section-wrapper">
+      <div className="lighthouse-words-container">
+        <div className="backslash" />
+        <div className="lighthouse-words">
+          <h4>13.32</h4>
+          <h4>who seeks after it and</h4>
+          <h4>wants to have it</h4>
+        </div>
+        <div className="slash" />
+      </div>
+      <img src="/assets/images/lighthouse.svg" alt="lighthouse" />
+    </div>
+  );
+};
+
+export default Lighthouse;
