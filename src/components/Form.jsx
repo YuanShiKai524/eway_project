231e91e36c00601a331396267467f8d1eb12edeb
yuanshikai524 <gcobc04992@gmail.com>
2023-01-25 06:45:52 +0800

    將表單輸入框改成用遍歷的方式展示，並稍微改變scss內容，以及添加輸入框錯誤提示的樣式

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,12 +1,89 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { nanoid } from 'nanoid';
 
 const Form = () => {
+  const handleChange = (e) => {
+    console.log(e.target.value);
+  };
+
+  const [fields] = useState([
+    {
+      label: 'store',
+      placeholder: 'store2',
+      onChange: handleChange,
+      list: 'stores',
+      options: ['store1', 'store2', 'store3', 'store113', 'store223'],
+      errMsg: '',
+    },
+    {
+      label: 'name',
+      placeholder: 'John Doe',
+      onChange: handleChange,
+      errMsg: '',
+    },
+    {
+      label: 'phone',
+      placeholder: '0910777888',
+      onChange: handleChange,
+      errMsg: '',
+    },
+    {
+      label: 'Amount of consumption',
+      placeholder: '10000',
+      onChange: handleChange,
+      errMsg: '',
+    },
+    {
+      label: 'payment',
+      defaultValue: 'digital payment',
+      onChange: handleChange,
+      list: 'payments',
+      options: ['digital payment', 'ATM'],
+      errMsg: '',
+    },
+  ]);
+
   const handleSubmit = (e) => {
     e.preventDefault();
   };
-  const handleChange = (e) => {
-    console.log(e.target.value);
+
+  // 此函式用於獲取輸入欄的html元素(包括其props)
+  const getElements = (field) => {
+    if (field.label === 'store' || field.label === 'payment') {
+      return (
+        <>
+          {field.label === 'store' ? (
+            <input
+              list={field.list}
+              name={field.label}
+              placeholder={field.placeholder}
+              onChange={field.onChange}
+            />
+          ) : (
+            <input
+              list={field.list}
+              name={field.label}
+              defaultValue={field.defaultValue}
+              onChange={field.onChange}
+            />
+          )}
+          <datalist id={field.list}>
+            {field.options.map((option) => {
+              return (
+                <option key={nanoid()} aria-label={option} value={option} />
+              );
+            })}
+          </datalist>
+        </>
+      );
+    }
+    return (
+      <input
+        name={field.label}
+        placeholder={field.placeholder}
+        onChange={field.onChange}
+      />
+    );
   };
 
   return (
@@ -23,79 +100,16 @@ const Form = () => {
           </div>
           <img src="/assets/images/turtle.svg" alt="turtle" />
         </div>
-        <label htmlFor="store">
-          <div>
-            store&nbsp;
-            <span>&#042;</span>
-          </div>
-          <input
-            list="stores"
-            name="store"
-            placeholder="store2"
-            onChange={handleChange}
-          />
-        </label>
-        <datalist id="stores">
-          {[1, 2, 3, 113, 223].map((num) => (
-            <option
-              key={nanoid()}
-              aria-label={`store${num}`}
-              value={`store${num}`}
-            />
-          ))}
-        </datalist>
-        <label htmlFor="name">
-          <div>
-            name&nbsp;
-            <span>&#042;</span>
-          </div>
-          <input
-            type="text"
-            name="name"
-            placeholder="John Doe"
-            onChange={handleChange}
-          />
-        </label>
-        <label htmlFor="phone">
-          <div>
-            phone&nbsp;
-            <span>&#042;</span>
-          </div>
-          <input
-            type="tel"
-            name="phone"
-            placeholder="0910777888"
-            onChange={handleChange}
-          />
-        </label>
-        <label htmlFor="consumption">
-          <div>
-            Amount of consumption&nbsp;
-            <span>&#042;</span>
-          </div>
-          <input
-            type="text"
-            name="consumption"
-            placeholder="10000"
-            onChange={handleChange}
-          />
-        </label>
-        <label htmlFor="payment">
-          <div>
-            payment&nbsp;
-            <span>&#042;</span>
-          </div>
-          <input
-            list="payments"
-            name="payment"
-            defaultValue="digital payment"
-            onChange={handleChange}
-          />
-        </label>
-        <datalist id="payments">
-          <option aria-label="digital payment" value="digital payment" />
-          <option aria-label="ATM" value="ATM" />
-        </datalist>
+        {fields.map((field) => (
+          <label htmlFor={field.label} key={field.label}>
+            <div>
+              {field.label}&nbsp;
+              <span>&#042;</span>
+            </div>
+            {getElements(field)}
+            <div className="caption errMsg">{field.errMsg}</div>
+          </label>
+        ))}
       </form>
       <button type="submit" form="myForm">
         <img src="/assets/images/submit.svg" alt="submit button" />
